f250bba7a05bae0100b7dcaaca8574bc20e540ce
Bowen <376512291@qq.com>
2019-11-21 22:12:34 +0800

    User中JWT登录

---
+++ tensquare_user/src/main/java/com/bowen/user/UserApplication.java
@@ -0,0 +1,33 @@
+package com.bowen.user;
+
+import com.bowen.common.util.IdWorker;
+import com.bowen.common.util.JwtUtil;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+    @Bean
+    public IdWorker idWorkker() {
+        return new IdWorker(1, 1);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bcryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JwtUtil jwtUtil() {
+        return new JwtUtil();
+    }
+}
